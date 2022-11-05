b9d7f3f12683a98e387e765a4554f1fd35b30198
Pigzi1 <117486257+Pigzi1@users.noreply.github.com>
2022-11-05 13:21:41 +0200

    score change

--- app/src/main/java/com/example/aplic/ScoreThread.java
+++ app/src/main/java/com/example/aplic/ScoreThread.java
@@ -2,12 +2,10 @@ package com.example.aplic;
 
 public class ScoreThread extends Thread{
     private boolean running = false;
-    private long lastUpdate;
     private GamePanel gamePanel;
 
     public void setRunning(boolean running) {
         this.running = running;
-        this.lastUpdate = 1000;
     }
 
     // מטרת התרד היא לספור את הניקוד ולהוסיף 500 בכל שנייה
@@ -16,11 +14,12 @@ public class ScoreThread extends Thread{
     }
     @Override
     public void run() {
-        lastUpdate = System.currentTimeMillis();
         while (running) {
-            if (System.currentTimeMillis() - lastUpdate > 1000) {
-                lastUpdate = System.currentTimeMillis();
+            try {
+                Thread.sleep(1000);
                 gamePanel.scoreUpdater();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
